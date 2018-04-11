05ab047dc765d1b5fd13fbd194c582d8cafd7472
elizabeth <elizabeth@qburst.com>
2018-04-11 12:48:12 +0530

    adding files to gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# cssDesignDemo
-
-* This is a sample HTML/CSS project which implements HTML caching mechanism and various CSS concepts rem, BEM etc.
---
+++ precache-manifest.7363e3dc02daa180437416657ad9b437.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25795a8da1b7688565c",
+    "url": "main.js"
+  },
+  {
+    "revision": "761a42b4796477e2044256de22060d1e",
+    "url": "index.html"
+  }
+];
